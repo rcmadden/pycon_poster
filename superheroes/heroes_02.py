c599330ca155c63e8fd1ecc3042e634436da4a5c
Russia Madden <dataengineer007@gmail.com>
2016-05-12 21:24:29 -0700

    fix dict values so they display add (almost working) template for guess name

--- superheroes/heroes_02.py
+++ superheroes/heroes_02.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from werkzeug.debug import DebuggedApplication
 from marvel_characters import character_info, character_images
+import random
 
 app = Flask(__name__)
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
@@ -11,5 +12,28 @@ def index():
     heroes = character_images.keys()
     return render_template('index_02.html', heroes=heroes, character_info=character_info, character_images=character_images)
 
+
+@app.route('/question')
+def show_question():
+    hero = random.choice(list(character_images.keys()))
+    heroes = character_images.keys()
+    return render_template('index_03.html', hero=hero, character_images=character_images, heroes=heroes, character_info=character_info)
+
+
+@app.route('/answer', methods=['POST'])
+def show_answer():
+    hero = random.choice(list(character_images.keys()))
+    heroes = character_images.keys()
+    answer = request.form['answer']
+    the_hero = request.form['hero']
+    # raise Exception
+    if request.form['answer'] == request.form['hero']:
+        correct = True
+    else:
+        correct = False
+    return render_template('index_03.html', hero=hero, character_images=character_images, heroes=heroes, character_info=character_info, correct=correct, answer=answer, the_hero=the_hero)
+
+
 if __name__ == "__main__":
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
--- superheroes/marvel_characters.py
+++ superheroes/marvel_characters.py
@@ -10,12 +10,12 @@ character_images = {
 
 character_info = {
     'Spider-Man': {'Real Name': 'Peter Parker', 'Powers': ''},
-    'Captain Marvel': {'Real Name': 'Billy Batson', 'Powers': ''},
+    'Captain Marvel': {'Real Name': 'Carol Danvers', 'Powers': ''},
     'Hulk': {'Real Name': 'Robert Banner', 'Powers': ''},
-    'Iron Man': {'Real Name': '', 'Powers': ''},
+    'Iron Man': {'Real Name': 'Tony Stark', 'Powers': ''},
     'Captain America': {'Real Name': 'Steven Rogers', 'Powers': ''},
     'Wolverine': {'Real Name': 'James Howlett',
                   'Powers': 'Wolverine is a mutant who possesses the ability to regenerate damaged or destroyed areas of his cellular structure at a rate far greater than that of an ordinary human.'},
-    'Ant-Man': {'Real Name': ('Scott Lang', 'Eric OGrady'),
+    'Ant-Man': {'Real Name': ('Scott Lang', 'Eric O`Grady'),
                 'Powers': 'Has the ability to shrink to roughly the size of an ant, also can communicate telepathically with insects, and (as Ant-Man) can punch with as much force as a normal sized person'}
 }
